e945a522a1d35fcf33fdf4d09989526cf49ae3fd
Nick <zagis-net@mail.ru>
2020-04-28 23:09:21 +0300

    Updates

---
+++ precache-manifest.e2b4e348eadcf818152f3e3346168b7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b7570b726ec43199c27b502a521b000",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fd136e433e0645a162fb",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9adb29d2dfd3f1d1e281",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fd136e433e0645a162fb",
+    "url": "./static/js/2.adda87dd.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.adda87dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9adb29d2dfd3f1d1e281",
+    "url": "./static/js/main.bb46e1eb.chunk.js"
+  },
+  {
+    "revision": "7501c352dbf2dac629bb",
+    "url": "./static/js/runtime-main.809dce56.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
