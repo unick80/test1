6acbbba161dd9140211d0b472f4ba4dbd3dfde89
Nick Pisarev <zagis-net@mail.ru>
2020-05-15 16:30:19 +0300

    Меняем Persika на котяру

--- src/panels/Persik.js
+++ src/panels/Persik.js
@@ -19,9 +19,9 @@ const Persik = props => (
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</PanelHeaderButton>}
 		>
-			Persik
+			Котяра
 		</PanelHeader>
-		<img className="Persik" src={persik} alt="Persik The Cat"/>
+		<img className="Persik" src={persik} alt="Это котяра"/>
 	</Panel>
 );
 
