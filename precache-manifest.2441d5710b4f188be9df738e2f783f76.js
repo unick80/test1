6a74e3e94a20fd311238a43f76151f99f1fc8333
Nick <zagis-net@mail.ru>
2020-05-18 19:22:59 +0300

    Updates

---
+++ precache-manifest.2441d5710b4f188be9df738e2f783f76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf7785b6787a5e79640a025edc9b9a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cbf6ac59ff908a84f741",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7008203add9c3e023b09",
+    "url": "./static/css/main.8a92ff14.chunk.css"
+  },
+  {
+    "revision": "cbf6ac59ff908a84f741",
+    "url": "./static/js/2.68986d39.chunk.js"
+  },
+  {
+    "revision": "d46a5294db188ba02afb1f5d9eabe773",
+    "url": "./static/js/2.68986d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7008203add9c3e023b09",
+    "url": "./static/js/main.274a83bc.chunk.js"
+  },
+  {
+    "revision": "7501c352dbf2dac629bb",
+    "url": "./static/js/runtime-main.809dce56.js"
+  },
+  {
+    "revision": "33774debd8815d931797fb6a235beeee",
+    "url": "./static/media/Cat.33774deb.jpg"
+  },
+  {
+    "revision": "f94fa09bb764915c89f96aaa0be4c3fd",
+    "url": "./static/media/Rabbit.f94fa09b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f04a1a8233b8f0208229cd7ec8102d82.js"
+  "./precache-manifest.2441d5710b4f188be9df738e2f783f76.js"
 );
 
 self.addEventListener('message', (event) => {
